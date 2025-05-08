6923a1c80f51611658952c26bf5f5a9ae4037244
arjunkmrm <arjunkmrm@gmail.com>
2025-05-07 17:06:49 -0700

    test update

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ import tools.pub_sub
 from common.config import MCP_TRANSPORT
 
 # test update
+# another test update
 class RedisMCPServer:
     def __init__(self):
         print("Starting the RedisMCPServer", file=sys.stderr)
